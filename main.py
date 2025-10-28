2df8f7cac924b1ca83d3a1d6586ad9c994d506c7
Juraj Kvasnak <kvasnakjuraj2112@gmail.com>
2025-10-28 11:58:46 +0100

    readme

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from tvary import obdlznik
 import tvary.kruh
 
 print(f"Obvod štvorca o veľkosti 20 je {stvorec.obvod(20)}")
-print(f"Obvod obdĺžnika o veľkosti 20 x 45 je {obdlznik.obvod(20, 45)}")
+print(f"Obvod obdĺžnika o veľkosti 20 x 45 je {obdlznik.obvod(20, 5)}")
 print(f"Obvod kruhu o veľkosti 60 je {tvary.kruh.obvod(60)}")
 
